a5b7c012eb65fe78dd6c1e47efa9a56e8a1c06c6
alex-streza <alex.streza@okapistudio.com>
2021-07-01 00:05:51 +0300

    changes

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -36,9 +36,8 @@ const sendEmail = (req: NextApiRequest, res: NextApiResponse) => {
   };
 
   transporter.sendMail(mailData, function (err: any, info: any) {
-    res.status(500).end(JSON.stringify(err));
-    // if (err) res.status(500).end(JSON.stringify(err));
-    // else res.status(200).end("Email sent successfully!");
+    if (err) res.status(500).end(JSON.stringify(err));
+    else res.status(200).end("Email sent successfully!");
   });
 };
 
